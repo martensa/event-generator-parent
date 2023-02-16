c2e8fe1ffe31341986c3a236a6931d3514ff8c8f
Alexander Martens <kontakt@alexandermartens.de>
2023-02-17 00:35:09 +0100

    Adding Event Consumer!

--- eventportal-event-generator/src/main/java/com/solace/amartens/eventportal/v2/eventgenerator/SolaceEventGenerator.java
+++ eventportal-event-generator/src/main/java/com/solace/amartens/eventportal/v2/eventgenerator/SolaceEventGenerator.java
@@ -9,17 +9,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SolaceEventGenerator {
     public static void main(String[] args) {
-        System.setProperty("otel.resource.attributes","service.name=Solace_Event_Generator");
-        System.setProperty("otel.exporter.otlp.endpoint", "http://localhost:4317");
-        System.setProperty("otel.traces.exporter","otlp");
-        System.setProperty("otel.metrics.exporter","logging");
-        System.setProperty("otel.logs.exporter","logging");
-        System.setProperty("otel.propagators","solace_jms_tracecontext");
-        System.setProperty("otel.bsp.schedule.delay","500");
-        System.setProperty("otel.bsp.max.queue.size","1000");
-        System.setProperty("otel.bsp.max.export.batch.size","5");
-        System.setProperty("otel.bsp.export.timeout","10000");
-
         SpringApplication.run(SolaceEventGenerator.class, args);
     }
 
